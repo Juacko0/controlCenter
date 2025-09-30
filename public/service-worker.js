d263dde8fc6f45a2af838ebb82581e8c50d22095
Juacko0 <jdelacruzramos0@gmail.com>
2025-09-30 17:18:25 -0500

    Primera PWA Actualizando

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activado");
+});
+
+// Recepción de notificaciones push
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icons/icon-192.png"
+    })
+  );
+});
